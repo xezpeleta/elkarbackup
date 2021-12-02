6eb402baed7aadb167b14ab10d7d303336aabc9b
Xabi Ezpeleta <xezpeleta@gmail.com>
2021-12-02 22:51:16 +0100

    Revert changes

--- run-tests.sh
+++ run-tests.sh
@@ -6,20 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $DIR/bin/console doctrine:database:drop --force
 $DIR/bin/console doctrine:database:create
 $DIR/bin/console doctrine:migrations:migrate --no-interaction
-
-if [ "$EUID" -ne 0 ]; then
-  echo "Checking permissions... sudo required:"
-  pwd
-  ls composer.json
-  sudo --preserve-env $DIR/bin/console elkarbackup:create_admin
-else
-  echo "Checking permissions... already root!"
-  # do not use sudo (GitHub Actions does not like it)
-  pwd
-  ls composer.json
-  $DIR/bin/console elkarbackup:create_admin
-fi
-
+sudo --preserve-env $DIR/bin/console elkarbackup:create_admin 
 mkdir -p /tmp/elkarbackup-tests/uploads
 $DIR/bin/console hautelook:fixtures:load --append
 $DIR/bin/phpunit "${@:1}"
